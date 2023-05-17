fc6d5478f0ff1f0867935ba152fea4e68fcd5173
goran-kovacic <kovacic.go@gmail.com>
2023-05-17 12:21:59 +0200

    zadatak_1

--- zadatak_1.sql
+++ zadatak_1.sql
@@ -1,3 +1,5 @@
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\AMD\Documents\GitHub\ER-diagram-zadaci\zadatak_1.sql
+
 drop database if exists zadatak_1;
 create database zadatak_1;
 use zadatak_1;
@@ -36,4 +38,20 @@ alter table djelatnica add foreign key (korisnik) references korisnik(sifra);
 alter table korisnik add foreign key (osoba) references osoba(sifra);
 
 alter table korisnik add foreign key (usluga) references usluga(sifra);
+
+insert into osoba (sifra,ime,prezime,telefon) values
+(null,'ime1','prezime1',null),
+(null,'ime1','prezime1',null),
+(null,'ime1','prezime1',null);
+
+insert into usluga (sifra, naziv,cijena) values
+(null,'usluga1',150),
+(null,'usluga2',200),
+(null,'usluga3',45);
+
+insert into korisnik (osoba,usluga) values
+(2,1),(3,3);
+
+insert into djelatnica (osoba,korisnik) values
+(1,2),(1,1);
 
